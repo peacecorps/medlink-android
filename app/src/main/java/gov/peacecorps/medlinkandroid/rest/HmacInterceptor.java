87c7859d8cfc2ab3e49bccd32c3ba1eecbde1bd9
Raj Rao <neeraj2608@gmail.com>
2016-03-02 14:42:23 -0500

    Attempting to fix 401s caused by incorrect auth header generation.

--- app/src/main/java/gov/peacecorps/medlinkandroid/rest/HmacInterceptor.java
+++ app/src/main/java/gov/peacecorps/medlinkandroid/rest/HmacInterceptor.java
@@ -1,5 +1,8 @@
 package gov.peacecorps.medlinkandroid.rest;
 
+import android.util.Log;
+
+import com.squareup.okhttp.Headers;
 import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -10,6 +13,7 @@ import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import gov.peacecorps.medlinkandroid.BuildConfig;
 import gov.peacecorps.medlinkandroid.helpers.AppSharedPreferences;
 import gov.peacecorps.medlinkandroid.helpers.HmacSigner;
 import okio.Buffer;
@@ -19,6 +23,7 @@ public class HmacInterceptor implements Interceptor {
 
     private final static String CONTENT_TYPE_APPLICATION_JSON = "application/json";
     private final static SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.US);
+    private static final String TAG = HmacInterceptor.class.getSimpleName();
 
     private final AppSharedPreferences appSharedPreferences;
     private final HmacSigner hmacSigner;
@@ -33,25 +38,38 @@ public class HmacInterceptor implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
         if (appSharedPreferences.hasUser()) {
-            return chain.proceed(augmentRequestWithHeaders(request));
+            return chain.proceed(augmentRequestWithAuthenticationHeaders(request));
         }
 
         return chain.proceed(request);
     }
 
-    private Request augmentRequestWithHeaders(Request request) throws IOException {
+    private Request augmentRequestWithAuthenticationHeaders(Request request) throws IOException {
         String md5ContentHash = getContentMD5Hash(request);
         String date = dateFormat.format(Calendar.getInstance().getTime());
         String plainText = CONTENT_TYPE_APPLICATION_JSON + "," + md5ContentHash + "," + request.url().getPath() + "," + date;
         String authHeader = "APIAuth " + hmacSigner.getHmac(plainText);
 
-        return request
+        Request augmentedRequest = request
                 .newBuilder()
                 .addHeader("Content-Type", CONTENT_TYPE_APPLICATION_JSON)
                 .addHeader("Content-MD5", md5ContentHash)
                 .addHeader("Date", date)
                 .addHeader("Authorization", authHeader)
                 .build();
+
+        if (BuildConfig.DEBUG) {
+            logHeaders(augmentedRequest);
+        }
+
+        return augmentedRequest;
+    }
+
+    private void logHeaders(Request request) {
+        Headers headers = request.headers();
+        for (String headerName : headers.names()) {
+            Log.d(TAG, String.format("%s: %s", headerName, headers.get(headerName)));
+        }
     }
 
     private String getContentMD5Hash(Request request) throws IOException {
@@ -59,7 +77,13 @@ public class HmacInterceptor implements Interceptor {
             final Buffer buffer = new Buffer();
             request.body().writeTo(buffer);
 
-            return ByteString.of(buffer.readByteArray()).md5().base64();
+            byte[] payloadBytes = buffer.readByteArray();
+
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, String.format("Payload: %s", new String(payloadBytes)));
+            }
+
+            return ByteString.of(payloadBytes).md5().base64();
         } else {
             return "";
         }
